5b7902fe33f99cc3f822f26f7d2d22317234e9e8
yellappam <yellappa.madigonde@foodstuffs.co.nz>
2017-12-27 21:39:10 +0000

    Step 9: Component Configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,24 +7,6 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 
 	return Controller.extend("sap.ui.demo.wt.controller.App", {
 
-		onInit: function() {
-			// set data model on view
-			var oData = {
-				recipient: {
-					name: "World"
-				}
-			};
-
-			var oModel = new JSONModel(oData);
-			this.getView().setModel(oModel);
-
-			// set i18n model on view
-			var i18nModel = new sap.ui.model.resource.ResourceModel({
-				bundleName: "sap.ui.demo.wt.i18n.i18n"
-			});
-			this.getView().setModel(i18nModel, "i18n");
-		},
-
 		onShowHello: function() {
 			 // read msg from i18n model
 			 var oBundle = this.getView().getModel("i18n").getResourceBundle();
