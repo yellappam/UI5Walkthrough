184d3dae8e503c6daba05916a21024e2d89f4b25
yellappam <yellappa.madigonde@foodstuffs.co.nz>
2017-12-27 21:21:14 +0000

    Step 8: Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,15 +10,27 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 		onInit: function() {
 			// set data model on view
 			var oData = {
-				recipient: { name: "World" }
+				recipient: {
+					name: "World"
+				}
 			};
 
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
+
+			// set i18n model on view
+			var i18nModel = new sap.ui.model.resource.ResourceModel({
+				bundleName: "sap.ui.demo.wt.i18n.i18n"
+			});
+			this.getView().setModel(i18nModel, "i18n");
 		},
 
 		onShowHello: function() {
-			MessageToast.show("Hello World");
+			 // read msg from i18n model
+			 var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			 var sMsg = oBundle.getText("helloMsg",sRecipient);
+			 MessageToast.show(sMsg);
 		}
 	});
 });
